ca1c6a11967b542cba625d50d28e6afdb6cfcafb
RagavRyan365 <ragavryan2008@gmail.com>
2026-08-03 15:12:42 +0530

    Add:Touch up in signup and login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,12 +4,14 @@ import {useRouter} from "next/navigation";
 export default function Login() {
   const router = useRouter();
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex flex-col gap-7 justify-center items-center">
+      <h1 className="text-3xl font-bold">Login</h1>
       <form className="flex flex-col gap-10">
         <input type="email" placeholder="Email" className="Input" required name="Email" />
         <input type="password" placeholder="Password" className="Input" required name="Password" />
-        <button type="submit">Login</button>
+        <button type="submit" className="bg-blue-300 w-auto h-8 rounded-md text-black">Login</button>
       </form>
+      <p>Don't have an account? <span onClick={()=>router.push("/signup")} className="text-blue-300 cursor-pointer">Signup</span></p>
     </div>
   );
 }
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -4,14 +4,16 @@ import {useRouter} from "next/navigation";
 export default function Signup() {
   const router = useRouter();
   return (
-    <div className="h-screen w-screen flex  gap-10 justify-center items-center">
+    <div className="h-screen w-screen flex flex-col gap-7 justify-center items-center">
+      <h1 className="text-3xl font-bold">Signup</h1>
       <form className="flex flex-col gap-10">
         <input type="text" placeholder="Username" className="Input" required name="Username" />
         <input type="email" placeholder="Email" className="Input" required name="Email" />
         <input type="password" placeholder="Password" className="Input" required name="Password" />
         <input type="password" placeholder="Confirm Password" className="Input" required name="ConfirmPassword" />
-        <button type="submit">Signup</button>
+        <button type="submit" className="bg-blue-300 w-auto h-8 rounded-md text-black">Signup</button>
       </form>
+      <p>Already have an account? <span onClick={()=>router.push("/login")} className="text-blue-300 cursor-pointer">Login</span></p>
     </div>
   );
 }
