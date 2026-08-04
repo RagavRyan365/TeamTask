4816fc039bee00b42d5de9f2e4878c9d8e5ada27
Ragav Ryan <ragavryan2008@gmail.com>
2026-08-04 18:21:33 +0530

    color fix in home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   }
   return (
     <div className="h-screen w-screen overflow-auto flex flex-col gap-5 justify-center items-center">
-      <h1 className="text-3xl font-bold my-10">Welcome to TeamTask</h1>
-      <button onClick={()=>router.push("/signup")} className="w-40 border-[var(--button-color)] border-1 rounded-[8px] px-5 py-1 transition-all duration-300 hover:bg-[var(--button-color)] hover:rounded-[15px] hover:text-black">Signup</button>
-      <button onClick={()=>router.push("/login")} className="w-40 border-[var(--button-color)] border-1 rounded-[8px] px-5 py-1 transition-all duration-300 hover:bg-[var(--button-color)] hover:rounded-[15px] hover:text-black">Login</button>
+      <h1 className="text-3xl dark:text-white light:text-black font-bold my-10">Welcome to TeamTask</h1>
+      <button onClick={()=>router.push("/signup")} className="w-40 border-[var(--button-color)] border-1 dark:text-white light:text-black rounded-[8px] px-5 py-1 transition-all duration-300 hover:bg-[var(--button-color)] hover:rounded-[15px] hover:text-black">Signup</button>
+      <button onClick={()=>router.push("/login")} className="w-40 border-[var(--button-color)] border-1 dark:text-white light:text-black rounded-[8px] px-5 py-1 transition-all duration-300 hover:bg-[var(--button-color)] hover:rounded-[15px] hover:text-black">Login</button>
     </div>
   );
 }
