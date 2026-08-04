426f5c9b185a8f9ad1bd2009cf42dcbb250be635
Ragav Ryan <ragavryan2008@gmail.com>
2026-08-04 17:21:06 +0530

    color correction

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -5,13 +5,13 @@ export default function Login() {
   const router = useRouter();
   return (
     <div className="h-screen w-screen overflow-auto flex flex-col gap-7 justify-center items-center">
-      <h1 className="text-3xl font-bold">Login</h1>
+      <h1 className="text-3xl light:text-black dark:text-white font-bold">Login</h1>
       <form className="flex flex-col gap-10">
         <input type="email" placeholder="Email" className="Input" required name="Email" />
         <input type="password" placeholder="Password" className="Input" required name="Password" />
-        <button type="submit" className="bg-blue-300 w-auto h-8 rounded-md !text-black">Login</button>
+        <button type="submit" className="bg-blue-300 w-auto h-8 rounded-md text-black">Login</button>
       </form>
-      <p>Don't have an account? <span onClick={()=>router.push("/signup")} className="text-blue-300 cursor-pointer">Signup</span></p>
+      <p className="dark:text-black light:text-white">Don't have an account? <span onClick={()=>router.push("/signup")} className="text-blue-300 cursor-pointer">Signup</span></p>
     </div>
   );
 }
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -5,7 +5,7 @@ export default function Signup() {
   const router = useRouter();
   return (
     <div className="h-screen w-screen overflow-auto flex flex-col gap-7 justify-center items-center">
-      <h1 className="text-3xl font-bold">Signup</h1>
+      <h1 className="text-3xl light:text-black dark:text-white font-bold">Signup</h1>
       <form className="flex flex-col gap-10">
         <input type="text" placeholder="Username" className="Input" required name="Username" />
         <input type="email" placeholder="Email" className="Input" required name="Email" />
@@ -13,7 +13,7 @@ export default function Signup() {
         <input type="password" placeholder="Confirm Password" className="Input" required name="ConfirmPassword" />
         <button type="submit" className="bg-blue-300 w-auto h-8 rounded-md !text-black">Signup</button>
       </form>
-      <p>Already have an account? <span onClick={()=>router.push("/login")} className="text-blue-300 cursor-pointer">Login</span></p>
+      <p className="dark:text-black light:text-white">Already have an account? <span onClick={()=>router.push("/login")} className="text-blue-300 cursor-pointer">Login</span></p>
     </div>
   );
 }
