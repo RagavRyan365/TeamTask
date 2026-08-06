862aae8db8658ffcd647d2dc30f3cbdd8622fa1e
Ragav Ryan <ragavryan2008@gmail.com>
2026-08-06 20:51:17 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -34,7 +34,7 @@ app.use(session({
     resave:false,
     saveUninitialized:true,
     cookie:{
-        secure:false,
+        secure:true,
         httpOnly:true,
         maxAge:1000*60*60*24*5,
     },
