fb1e5b84b5ba48343cf808a5bc5ec7cd9d09cf2e
Ragav Ryan <ragavryan2008@gmail.com>
2026-08-06 09:11:45 +0530

    test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   }
   return (
     <div className="h-screen w-screen overflow-auto flex flex-col gap-5 justify-center items-center">
-      <h1 className="text-3xl dark:text-white light:text-black font-bold my-10">Welcome to TeamTask</h1>
+      <h1 className="text-3xl dark:text-white light:text-black font-bold my-10">Welcome to TeamTaskCo</h1>
       <button onClick={()=>router.push("/signup")} className="w-40 border-[var(--button-color)] border-1 dark:text-white light:text-black rounded-[8px] px-5 py-1 transition-all duration-300 hover:bg-[var(--button-color)] hover:rounded-[15px] hover:text-black">Signup</button>
       <button onClick={()=>router.push("/login")} className="w-40 border-[var(--button-color)] border-1 dark:text-white light:text-black rounded-[8px] px-5 py-1 transition-all duration-300 hover:bg-[var(--button-color)] hover:rounded-[15px] hover:text-black">Login</button>
     </div>
